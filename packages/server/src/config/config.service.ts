3d6dbcaea2f72f7717539d2b8c9992a4d279d242
luis <luisjosegonzaleztovar@gmail.com>
2019-05-23 12:59:11 -0400

    fixed  UPLOAD_PATH

--- packages/server/src/config/config.service.ts
+++ packages/server/src/config/config.service.ts
@@ -27,18 +27,20 @@ export class ConfigService {
         .default('development'),
       APP_PORT: Joi.number().default(3000),
       APP_SECRET_KEY: Joi.string().required(),
+      
       DB_NAME_VAD10: Joi.string().required(),
       DB_USERNAME_VAD10: Joi.string().required(),
       DB_PASSWORD_VAD10: Joi.string().required(),
       DB_HOST_VAD10: Joi.string().required(),
       DB_PORT_VAD10: Joi.number().required(),
-      UPLOAD_PATH: Joi.string().required(),
-
+      UPLOAD_PATH_VAD10: Joi.string().required(),
+      
       DB_NAME_VAD20: Joi.string().required(),
       DB_USERNAME_VAD20: Joi.string().required(),
       DB_PASSWORD_VAD20: Joi.string().required(),
       DB_HOST_VAD20: Joi.string().required(),
       DB_PORT_VAD20: Joi.number().required(),
+      UPLOAD_PATH_VAD20: Joi.string().required(),
 
     });
 
