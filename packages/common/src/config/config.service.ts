26e5aec2078973f2b64c411232b734b6c86ffd6a
luis <luisjosegonzaleztovar@gmail.com>
2019-05-27 11:42:11 -0400

    fixed seeder

--- packages/common/src/config/config.service.ts
+++ packages/common/src/config/config.service.ts
@@ -15,8 +15,6 @@ export class ConfigService {
 
   constructor(filePath: string) {
     const config = dotenv.parse(fs.readFileSync(filePath));
-    console.log(config);
-    
     this.envConfig = this.validateInput(config);
   }
 
